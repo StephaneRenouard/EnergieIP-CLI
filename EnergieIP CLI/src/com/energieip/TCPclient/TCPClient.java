1d073e98b20b34a5f06d6786ccf30002534777fe
StephaneRenouard <stephane.renouard@gmail.com>
2017-11-05 18:43:10 +0100

    add TCP client

---
+++ EnergieIP CLI/src/com/energieip/TCPclient/TCPClient.java
@@ -0,0 +1,26 @@
+package com.tcp.client;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class TCPClient {
+		 public static void main(String argv[])
+		 {
+		  String sentence = System.currentTimeMillis() + ": Test Message from TCP Client";
+		  Socket clientSocket = null;
+		try {
+			clientSocket = new Socket("localhost", 5555);
+
+		  DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		  outToServer.writeBytes(sentence);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		  finally {try {
+			if (clientSocket != null) clientSocket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}}
+		 }
+		}
